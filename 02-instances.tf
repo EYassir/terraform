2a3f9ac44f3f6742af69dd1bf315706c9e803a28
cyber <yassir@cyber.localdomain>
2021-11-18 15:35:53 +0100

    Tag instance

--- 02-instances.tf
+++ 02-instances.tf
@@ -5,4 +5,7 @@ resource "aws_instance" "example" {
   key_name        = aws_key_pair.example_key.key_name
   user_data       = data.template_file.user_data.rendered
   security_groups = [aws_security_group.example_sg.name]
+  tags = {
+    "Name": "instance-${count.index}-${var.env}"
+  }
 }
